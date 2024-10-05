79b15de8d820102f4faef4bb8e9b7fe68ce897e5
avazbek489 <obidjonovavazbek787@gmail.com>
2024-10-05 19:50:24 +0500

    for and ternary question end

--- js/script.js
+++ js/script.js
@@ -1,13 +1,33 @@
-for (let num = 1; num <= 50; num++) {
-  let res;
-  if (num % 3 === 0 && num % 5 === 0) {
-    res = "5 va 3 ga bolinadi";
-  } else if (num % 5 == 0) {
-    res = "5 ga bolinadi";
-  } else if (num % 3 == 0) {
-    res = "3 ga bolinadi";
-  } else {
-    res = num;
-  }
-  console.log(res);
-}
+// // // For va ternary operatorga oid
+// // 1-MASALA JAVOBI
+// for (let i = 1; i <= 50; i++){
+//   i % 3 == 0 ? console.log(`3ga bolinadi ` + i) : console.log();
+//   i % 5 == 0 ? console.log(`5ga bolinadi ` + i) : console.log();
+//   i % 3 == 0 && i % 5 == 0 ? console.log(`3ga va 5ga bolinadi ` + i) : console.log();
+// }
+
+// // 2-MASALA JAVOBI
+// let sum = 0;
+// for(let i = 0; i <= 100; i += 2){
+// i >= 30 && i <= 70 ? sum += i : console.log();
+// }
+// alert(sum);
+
+// // 3-MASALA JAVOBI
+// let count = 0;
+// let neg = 0;
+// for (let i = -20; i <= 20; i++){
+// i % 2 == 0 ? count++ : neg++;
+// }
+// alert(`\nJuftlar soni ` + count + `\nToqlar soni ` + neg);
+
+// // 4-MASALA JAVOBI
+// for (let i = 1; i <= 100; i++) {
+//   i % 2 == 0 || i % 7 == 0 ? console.log(`Maxsus son ` + i) : ``;
+// }
+
+// // // Break continue ga oid
+
+// // 1-MASALA JAVOBI
+
+
